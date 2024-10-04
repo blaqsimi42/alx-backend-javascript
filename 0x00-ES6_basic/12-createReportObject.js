b26be7be74060d3ecaf4254f39a1fc32add4f47d
blaqsimi42 <cimee464@gmail.com>
2024-10-04 01:55:12 +0000

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,15 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
